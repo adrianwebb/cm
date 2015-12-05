84a7ac6d1b7677ca35b19bbf803d74c6730a8b2f
Adrian Webb <adrian.webb@coraltech.net>
2015-12-05 18:39:16 -0500

    Adding scaffolding for a base CM sequence plugin.

---
+++ lib/core/plugin/sequence.rb
@@ -0,0 +1,32 @@
+
+module CM
+module Plugin
+class Sequence < Nucleon.plugin_class(:nucleon, :base)
+
+  #-----------------------------------------------------------------------------
+  # Plugin interface
+
+  def normalize(reload)
+    super
+    yield if block_given?
+  end
+
+  #-----------------------------------------------------------------------------
+  # Checks
+
+  def initialized?(options = {})
+    true
+  end
+
+  #-----------------------------------------------------------------------------
+  # Property accessors / modifiers
+
+  #-----------------------------------------------------------------------------
+  # Operations
+
+  #-----------------------------------------------------------------------------
+  # Utilities
+
+end
+end
+end
