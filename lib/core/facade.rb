cdac857feda0afec47ed091a28047f259e83b652
Adrian Webb <adrian.webb@coraltech.net>
2015-12-05 15:42:21 -0500

    Adding configuration file properties to the CM facade.

--- lib/core/facade.rb
+++ lib/core/facade.rb
@@ -2,5 +2,19 @@
 module CM
 module Facade
 
+  #-----------------------------------------------------------------------------
+  # Configuration settings
+
+  def config_dir
+    ENV['CM_CONFIG_DIR'] || '/etc/cm'
+  end
+
+  def config_file
+    ENV['CM_CONFIG_FILE'] || 'config.yml'
+  end
+
+  def config_path
+    "#{config_dir}/#{config_file}"
+  end
 end
 end
