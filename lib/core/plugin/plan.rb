f0e6fa0e5b4c6d06990fb4e56ae1e84b4c245062
Adrian Webb <adrian.webb@coraltech.net>
2015-12-05 18:37:44 -0500

    Adding scaffolding for a CM plan base plugin.

---
+++ lib/core/plugin/plan.rb
@@ -0,0 +1,32 @@
+
+module CM
+module Plugin
+class Plan < Nucleon.plugin_class(:nucleon, :base)
+
+  #-----------------------------------------------------------------------------
+  # Plugin interface
+
+  def normalize(reload)
+    super
+    yield if block_given?
+  end
+
+  #-----------------------------------------------------------------------------
+  # Checks
+
+  def initialized?(options = {})
+    true
+  end
+
+  #-----------------------------------------------------------------------------
+  # Property accessors / modifiers
+
+  #-----------------------------------------------------------------------------
+  # Operations
+
+  #-----------------------------------------------------------------------------
+  # Utilities
+
+end
+end
+end
