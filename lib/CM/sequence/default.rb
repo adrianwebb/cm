57ff9666b9ce5c1f51a13f2b31dcdc59a44069aa
Adrian Webb <adrian.webb@coraltech.net>
2015-12-05 18:42:16 -0500

    Adding scaffolding for a default CM sequence plugin provider.

---
+++ lib/CM/sequence/default.rb
@@ -0,0 +1,32 @@
+
+module CM
+module Sequence
+class Default < Nucleon.plugin_class(:CM, :sequence)
+
+  #-----------------------------------------------------------------------------
+  # Plugin interface
+
+  def normalize(reload)
+    super
+    yield if block_given?
+  end
+
+  #-----------------------------------------------------------------------------
+  # Checks
+
+  def initialized?(options = {})
+    true
+  end
+
+  #-----------------------------------------------------------------------------
+  # Property accessors / modifiers
+
+  #-----------------------------------------------------------------------------
+  # Operations
+
+  #-----------------------------------------------------------------------------
+  # Utilities
+
+end
+end
+end
