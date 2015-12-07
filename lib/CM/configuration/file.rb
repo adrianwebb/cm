8430549b3555b2b8ae3a645ecc6f305d5cd4a761
Adrian Webb <adrian.webb@coraltech.net>
2015-12-07 01:25:41 -0500

    Splitting up file and directory base configuration handling between two configuration plugin providers.

--- lib/CM/configuration/file.rb
+++ lib/CM/configuration/file.rb
@@ -1,7 +1,7 @@
 
 module CM
 module Configuration
-class Directory < Nucleon.plugin_class(:CM, :disk_configuration)
+class File < Nucleon.plugin_class(:CM, :disk_configuration)
 
   #-----------------------------------------------------------------------------
   # Plugin interface
@@ -15,26 +15,18 @@ class Directory < Nucleon.plugin_class(:CM, :disk_configuration)
   # Checks
 
   def initialized?(options = {})
-    File.directory?(path)
+    file && File.exist?(file)
   end
 
   #-----------------------------------------------------------------------------
   # Property accessors / modifiers
-
-  def output_file
-    _get(:output_file, 'rendered.config.yaml')
-  end
-
-  def target_path
-    File.join(path, output_file)
-  end
 
   #-----------------------------------------------------------------------------
   # Operations
 
   def parse(wipe = true)
     super do
-      import_directory(path)
+      import_file(path)
     end
   end
 
@@ -42,29 +34,23 @@ class Directory < Nucleon.plugin_class(:CM, :disk_configuration)
 
   def save
     super do
-      save_config(target_path, export)
+      save_config(path, export)
     end
   end
 
   #-----------------------------------------------------------------------------
   # Utilities
 
-  def import_directory(directory, force = true, deep_merge = true)
+  def import_file(file, force = true, deep_merge = true)
     success = true
-
-    Nucleon.loaded_plugins(:nucleon, :translator).each do |provider, info|
-      Dir.glob(::File.join(directory, '**', "*.#{provider}")).each do |file|
-        logger.debug("Merging configurations from: #{file}")
-        if config_data = parse_config(file)
-          import(config_data, { :force => force, :basic => !deep_merge })
-        else
-          success = false
-        end
-      end
+    if config_data = parse_config(file)
+      import(config_data, { :force => force, :basic => !deep_merge })
+    else
+      success = false
     end
     success
   end
-  protected :import_directory
+  protected :import_file
 end
 end
 end
