a5a715c8ff216e3b7216f9a7fdb984827c7b4449
Adrian Webb <adrian.webb@coraltech.net>
2015-12-07 01:22:32 -0500

    Refactoring system configuration loading to work with files or directories in the config action mixin.

--- lib/core/mixin/action/config.rb
+++ lib/core/mixin/action/config.rb
@@ -8,22 +8,16 @@ module Config
   # Settings
 
   def config_config
-    register_str :config_dir, CM.config_dir, 'nucleon.mixin.action.config.options.config_dir' do |value|
+    register_str :config_path, CM.config_path, 'nucleon.mixin.action.config.options.config_path' do |value|
       success = true
 
       if value
-        directory = File.expand_path(value)
+        path = File.expand_path(value)
 
-        # Configuration directory exists
-        if File.exist?(directory)
-          # Get all configurations
-          import_system_config(directory)
-
-          # No configuration available?
-          success = false if @system_config.nil?
+        if File.exist?(path)
+          success = false unless import_system_config(path)
         else
-          # Non existent configuration directory
-          warn('nucleon.mixin.action.config.errors.config_dir', { :directory => directory })
+          warn('nucleon.mixin.action.config.errors.config_path', { :path => path })
           success = false
         end
       end
@@ -34,7 +28,7 @@ module Config
   #---
 
   def config_ignore
-    [ :config_dir ]
+    [ :config_path ]
   end
 
   #-----------------------------------------------------------------------------
@@ -43,43 +37,25 @@ module Config
   #-----------------------------------------------------------------------------
   # Utilities
 
-  def import_system_config(directory)
-    @system_config = {}
+  def import_system_config(path)
+    config_provider = (File.directory?(path) ? :directory : :file)
+    system_config = CM.configuration(extended_config(:system_config, {
+      :path => path,
+      :translator_error => 'nucleon.mixin.action.config.errors.translator',
+      :config_error => 'nucleon.mixin.action.config.errors.config_file'
+    }), config_provider).parse
 
-    # Merge all system configurations
-    Nucleon.loaded_plugins(:nucleon, :translator).each do |provider, info|
-      Dir.glob(::File.join(directory, '**', "*.#{provider}")).each do |file|
-        logger.debug("Merging system configurations from: #{file}")
-        if config_data = parse_config_file(file)
-          @system_config = Util::Data.merge([ @system_config, config_data ], true, false)
+    unless system_config.nil?
+      # Values are already set from parameters and validation is just starting
+      system_config.export.each do |key, value|
+        # TODO: Some error handling here if config key doesn't exist
+        # For instance, if extra properties in config file
+        if !config.has_key?(key) || settings[key] == config[key].default
+          settings[key] = value
         end
       end
     end
-
-    # Values are already set from parameters and validation is just starting
-    @system_config.each do |key, value|
-      # TODO: Some error handling here if config key doesn't exist
-      # For instance, if extra properties in config file
-      if !config.has_key?(key) || settings[key] == config[key].default
-        settings[key] = value
-      end
-    end
-  end
-
-  #---
-
-  def parse_config_file(file)
-    begin
-      provider   = File.extname(file).sub(/^\./, '')
-      translator = Nucleon.translator({}, provider) if provider
-      raise I18n.t('nucleon.mixin.action.config.errors.translator', { :provider => provider, :file => file }) unless translator
-      config_data = translator.parse(Util::Disk.read(file))
-    rescue => error
-      warn('nucleon.mixin.action.config.errors.config_file', { :file => file })
-      warn(error.message, { :i18n => false })
-      config_data = nil
-    end
-    config_data
+    system_config.nil? ? false : true
   end
 end
 end
