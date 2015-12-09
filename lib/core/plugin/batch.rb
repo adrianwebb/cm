07e535a1fcd5a0e67385f6f6cd1226a82cf02f02
Adrian Webb <adrian.webb@coraltech.net>
2015-12-09 18:32:07 -0500

    Adding functionality to the base batch plugin.

--- lib/core/plugin/batch.rb
+++ lib/core/plugin/batch.rb
@@ -12,7 +12,25 @@ class Batch < Nucleon.plugin_class(:nucleon, :parallel_base)
 
   def normalize(reload)
     super
+
+    @sequence = delete(:sequence, nil)
+
+    init_jobs
     yield if block_given?
+  end
+
+  #---
+
+  def init_jobs
+    @jobs = []
+    get_array(:jobs).each do |job_config|
+      if job_config.has_key?(:sequence) # Array
+        @jobs << sequence.create_sequence(job_config[:sequence])
+      else # Atomic
+        @jobs << sequence.create_job(job_config)
+      end
+    end
+    @jobs
   end
 
   #-----------------------------------------------------------------------------
@@ -25,12 +43,53 @@ class Batch < Nucleon.plugin_class(:nucleon, :parallel_base)
   #-----------------------------------------------------------------------------
   # Property accessors / modifiers
 
+  def sequence
+    @sequence
+  end
+
+  #---
+
+  def jobs
+    @jobs
+  end
+
+  def jobs=jobs
+    set(:jobs, array(jobs))
+    init_jobs
+  end
+
   #-----------------------------------------------------------------------------
   # Operations
+
+  def execute(settings, parallel = true)
+    if initialized?
+      if Nucleon.parallel? && parallel
+        success = execute_parallel
+      else
+        success = execute_sequence
+      end
+    else
+      success = false
+    end
+    success
+  end
 
   #-----------------------------------------------------------------------------
   # Utilities
 
+  def execute_parallel
+    false # Override me!!
+  end
+
+  #---
+
+  def execute_sequence
+    success = true
+    jobs.each do |job|
+      success = false unless job.execute(sequence.settings)
+    end
+    success
+  end
 end
 end
 end
