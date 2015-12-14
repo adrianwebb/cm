4d509da22422132edcc9cfe82b6902142a3c07f2
Adrian Webb <adrian.webb@coraltech.net>
2015-12-14 00:02:36 -0500

    Some unfinished refactoring of the base docker resource provider.

--- lib/core/plugin/docker_resource.rb
+++ lib/core/plugin/docker_resource.rb
@@ -14,6 +14,8 @@ class DockerResource < Nucleon.plugin_class(:CM, :resource)
     require 'docker'
     super
 
+    codes :docker_exec_failed
+
     settings[:docker_protocol] ||= 'unix'
     settings[:docker_sock] ||= '/var/run/docker.sock'
     settings[:docker_host] ||= nil
@@ -25,8 +27,6 @@ class DockerResource < Nucleon.plugin_class(:CM, :resource)
     else
       Docker.url = "#{settings[:docker_protocol]}://#{settings[:docker_host]}:#{settings[:docker_port]}"
     end
-
-    codes :docker_exec_failed
 
     yield if block_given?
   end
@@ -47,7 +47,7 @@ class DockerResource < Nucleon.plugin_class(:CM, :resource)
   #---
 
   def internal?
-    get(:internal, false)
+    File.exist?('/.dockerinit')
   end
 
   #-----------------------------------------------------------------------------
@@ -57,20 +57,10 @@ class DockerResource < Nucleon.plugin_class(:CM, :resource)
     get(:image, 'awebb/cm').to_s
   end
 
-  def image=image
-    set(:image, image)
-    @container = create_container(image, startup_commands)
-  end
-
   #---
 
   def startup_commands
     get(:startup_commands, ['bash'])
-  end
-
-  def startup_commands=commands
-    set(:startup_commands, array(commands))
-    @container = create_container(image, commands)
   end
 
   #---
@@ -79,89 +69,196 @@ class DockerResource < Nucleon.plugin_class(:CM, :resource)
     @container
   end
 
+  #---
+
+  def plan_directory
+    get(:plan_directory, '/opt/cm/volumes/plan')
+  end
+
+  def config_directory
+    get(:config_directory, '/opt/cm/volumes/config')
+  end
+
+  def token_directory
+    get(:token_directory, '/opt/cm/volumes/token')
+  end
+
+  def key_directory
+    get(:key_directory, '/opt/cm/volumes/keys')
+  end
+
+  #---
+
+  def host_input_directory
+    get(:host_input_directory, "/tmp/cm/input/#{plugin_instance_name}")
+  end
+
+  def input_directory
+    get(:input_directory, '/opt/cm/volumes/input')
+  end
+
+  #---
+
+  def host_output_directory
+    get(:host_output_directory, "/tmp/cm/output/#{plugin_instance_name}")
+  end
+
+  def output_directory
+    get(:output_directory, '/opt/cm/volumes/output')
+  end
+
   #-----------------------------------------------------------------------------
   # Operations
 
-  def execute
+  def operation_deploy
     super do
-      success = true
+      data = nil
 
+      # A fork in the road!
       if internal?
-        success = yield if block_given?
+        info("Yay!!! We are here", { :i18n => false })
+        data = yield if block_given?
       else
-        @container = create_container(image, startup_commands)
-
-        encoded_config = Util::CLI.encode(Util::Data.clean(settings))
-
-        action_config  = extended_config(:container_action, {
-          :command => provider,
-          :data    => { :encoded  => encoded_config }
-        })
-        action_config[:data][:log_level] = Nucleon.log_level if Nucleon.log_level
-
-        command_base  = 'cm'
-        command_base  = "NUCLEON_NO_PARALLEL=1 #{command_base}" unless Nucleon.parallel?
-        command_base  = "NUCLEON_NO_COLOR=1 #{command_base}" unless Nucleon::Util::Console.use_colors
-
-        command = Nucleon.command({
-          :command => command_base,
-          :data    => { 'git-dir=' => repo.path },
-          :subcommand => {
-            :command => command.to_s.gsub('_', '-'),
-            :flags   => flags,
-            :data    => data,
-            :args    => processed_args
-          }
-        }, command_provider)
-
-        results = container.exec(["cm resource exec #{}"], { :stdout => false }) do |stream, message|
-          render_docker_message(stream, message)
-        end
-        myself.status = code.docker_exec_failed unless results[2] == 0
+        data = action(plugin_provider, :deploy)
+        myself.status = code.docker_exec_failed unless data
       end
-      success
+      data
     end
+  end
+
+  #-----------------------------------------------------------------------------
+  # Docker resource operation execution
+
+  def exec(command)
+    data = nil
+
+    FileUtils.mkdir_p(host_output_directory)
+    create_container
+
+    results = container.exec(['bash', '-l', '-c', command]) do |stream, message|
+      unless message.match(/stdin\:\s+is not a tty/)
+        render_docker_message(stream, message)
+        yield(stream, message) if block_given?
+      end
+    end
+
+    if results[2] == 0
+      if output_config = CM.configuration(extended_config(:resource_results, {
+        :provider => get(:resource_result_provider, :directory),
+        :path => host_output_directory
+      }))
+        data = Nucleon::Util::Data.clone(output_config.export)
+        Nucleon.remove_plugin(output_config)
+      end
+    end
+
+    destroy_container
+    FileUtils.rm_rf(host_output_directory)
+    data
+  end
+
+  #---
+
+  def command(command, options = {})
+    config         = Nucleon::Config.ensure(options)
+    remove_command = false
+
+    unless command.is_a?(Nucleon::Plugin::Command)
+      command        = Nucleon.command(Nucleon::Config.new({ :command => command }, {}, true, false).import(config), :bash)
+      remove_command = true
+    end
+
+    data = exec(command.to_s.strip) do |stream, message|
+      yield(stream, message) if block_given?
+    end
+
+    Nucleon.remove_plugin(command) if remove_command
+    data
+  end
+
+  #---
+
+  def action(provider, operation)
+    encoded_config = Nucleon::Util::CLI.encode(Nucleon::Util::Data.clean(settings))
+    action_config  = extended_config(:action, {
+      :command => 'resource run',
+      :data    => { :encoded  => encoded_config },
+      :args    => [ provider, operation ]
+    })
+    action_config[:data][:log_level] = Nucleon.log_level if Nucleon.log_level
+
+    data = command('cm', Nucleon::Util::Data.clean({
+      :subcommand => action_config,
+      :quiet      => Nucleon::Util::Console.quiet
+    })) do |stream, message|
+      yield(stream, message) if block_given?
+    end
+    data
   end
 
   #-----------------------------------------------------------------------------
   # Utilities
 
-  def create_container(image, startup_commands)
+  def create_container
     container = nil
 
-    destroy_container(plugin_instance_name)
+    destroy_container
 
-    container = Docker::Container.create({
+    container_env = []
+    container_env << "NUCLEON_NO_PARALLEL=1" unless Nucleon.parallel?
+    container_env << "NUCLEON_NO_COLOR=1" unless Nucleon::Util::Console.use_colors
+
+    @container = Docker::Container.create({
       'name' => plugin_instance_name,
       'Image' => image,
       'Cmd' => array(startup_commands),
-      'Tty' => true
+      'Tty' => true,
+      'Env' => container_env,
+      'Volumes' => {
+        CM.config_path => {},
+        plan_directory => {},
+        config_directory => {},
+        token_directory => {},
+        key_directory => {},
+        input_directory => {},
+        output_directory => {}
+      },
+      'HostConfig' => {
+        'Binds' => [
+          "#{CM.config_path}:#{CM.config_path}:ro",
+          "#{plan.path}:#{plan_directory}:ro",
+          "#{plan.config_directory}:#{config_directory}:ro",
+          "#{plan.token_directory}:#{token_directory}:rw",
+          "#{plan.key_directory}:#{key_directory}:rw",
+          "#{host_input_directory}:#{input_directory}:ro",
+          "#{host_output_directory}:#{output_directory}:rw"
+        ]
+      }
     })
 
-    if container
-      container.start!
+    if @container
+      @container.start!
     else
       error('cm.resource.docker.error.container_failed', {
-        :command => command.to_s,
         :image => image
       })
     end
-    container
   end
   protected :create_container
 
 
   #---
 
-  def destroy_container(name)
+  def destroy_container
     containers = Docker::Container.all({ :all => 1 })
 
     containers.each do |cont|
-      if cont.info['Names'].include?("/#{name}")
+      if cont.info['Names'].include?("/#{plugin_instance_name}")
         cont.kill!
         cont.remove
       end
     end
+    @container = nil
   end
   protected :destroy_container
 
