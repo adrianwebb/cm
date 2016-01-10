936cf7af29364a394a4d6cc0f10a2cbedde4b903
Adrian Webb <adrian.webb@coraltech.net>
2016-01-10 11:51:54 -0500

    Early testing on the AWS resource provider.

--- lib/CM/resource/AWS.rb
+++ lib/CM/resource/AWS.rb
@@ -30,11 +30,29 @@ class AWS < Nucleon.plugin_class(:CM, :docker_resource)
 
   def operation_deploy
     super do
+      require 'fog/aws'
+
       if template.to_sym == :keypair
         info('deploy_keypair', { :id => id, :name => parameters[:Name], :prefix => false })
       else
         info('deploy_cloudformation', { :id => id, :prefix => false })
       end
+
+      manifest_config = plan.manifest_config
+
+      dbg(manifest_config, 'manifest config')
+      dbg(manifest_config[:aws][:AccessKey], 'aws access key')
+      dbg(manifest_config[:aws][:SecretAccessKey], 'aws secret access key')
+      dbg(manifest_config[:aws][:Region], 'aws region')
+
+      #compute = Fog::Compute.new({
+      #  :provider => 'AWS',
+      #  :region => manifest_config[:aws][:AccessKey],
+      #  :aws_access_key_id => manifest_config[:aws][:SecretAccessKey],
+      #  :aws_secret_access_key => manifest_config[:aws][:Region]
+      #})
+
+
       data = {}
     end
   end
