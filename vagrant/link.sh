937544a8d1edbbebc34ae295959344395608e176
Adrian Webb <adrian.webb@coraltech.net>
2016-01-10 13:38:26 -0500

    Adding sudo to the vagrant link script.

--- vagrant/link.sh
+++ vagrant/link.sh
@@ -3,7 +3,7 @@
 
 # Linking CM source directory
 echo "*** Cleaning old CM gem definitions"
-rm -Rf "$RUBY_GEM_PATH"/cm-*
+sudo rm -Rf "$RUBY_GEM_PATH"/cm-*
 
 echo "*** Linking CM"
-ln -s "$GEM_CM_DIRECTORY" "$RUBY_GEM_PATH/cm-$GEM_CM_VERSION" || exit 210
+sudo ln -s "$GEM_CM_DIRECTORY" "$RUBY_GEM_PATH/cm-$GEM_CM_VERSION" || exit 210
