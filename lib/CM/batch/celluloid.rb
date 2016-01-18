4348986521a68fc233bbea685987579a5c2f7b8e
Adrian Webb <adrian.webb@coraltech.net>
2016-01-17 21:19:31 -0500

    Rescue, error logging, and adding timeouts in the celluloid batch provider.

--- lib/CM/batch/celluloid.rb
+++ lib/CM/batch/celluloid.rb
@@ -25,12 +25,32 @@ class Celluloid < Nucleon.plugin_class(:CM, :batch)
 
   def execute_parallel(operation)
     values = []
+    timeouts = []
+
     resources.each do |resource|
       values << ::Celluloid::Future.new(resource) do
-        resource.execute(operation)
+        success = true
+        begin
+          timeouts << ( resource.respond_to?(:timeout) ? resource.timeout : nil )
+
+          resource.execute(operation)
+          success = resource.status == code.success
+
+        rescue => error
+          logger.error("Resource #{resource.id} #{operation} experienced an error:")
+          logger.error(error.inspect)
+          logger.error(error.message)
+          logger.error(Nucleon::Util::Data.to_yaml(error.backtrace))
+
+          error('resource_execution', { :id => resource.id, :operation => operation, :message => error.message })
+          success = false
+        end
+        success
       end
     end
-    values = values.map { |future| future.value }
+    values = values.each_with_index.map do |future, index|
+      future.value(timeouts[index])
+    end
     !values.include?(false)
   end
 end
